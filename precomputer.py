Fixed a bug in bispectrum covariance computation

precomputer.py
@@ -4,22 +4,25 @@ import pandas as pd
 
 
 p_max = 30
-Nks = [10, 50, 200]
+#Nks = [10, 50, 200]
+#Nks = [50,200]
+Nks = [10]
 
 for Nk in Nks:
     basis = best.Basis("Legendre", mode_p_max=30, polarisation_on=True, k_grid_size=Nk, use_tetraquad=False)
+    #basis = best.Basis("Legendre", mode_p_max=30, polarisation_on=True, k_grid_size=Nk, use_tetraquad=False, precomputed_QQ_path=f"data/mode_bispectra_covarinace_p_{p_max}_Nk_{Nk}.npy")
+    #basis = best.Basis("Legendre", mode_p_max=30, polarisation_on=True, k_grid_size=Nk, use_tetraquad=True)
 
     i1, i2, i3 = basis.tetrapyd_indices
     k1, k2, k3 = basis.tetrapyd_grid
     weights = basis.tetrapyd_grid_weights
     df = pd.DataFrame({"i1": i1, "i2": i2, "i3": i3, "k1": k1, "k2": k2, "k3": k3,
                             "weight": weights})
-    df.to_csv(f"data/uniform_tetrapyd_cython_Nk_{Nk}.csv", float_format="%.18e")
+    #df.to_csv(f"data/uniform_tetrapyd_cython_Nk_{Nk}.csv", float_format="%.18e")
 
     print(basis.data_path)
-    np.save(f"data/mode_bispectra_covarinace_p_{p_max}_Nk_{Nk}.npy", basis.mode_bispectra_covariance)
+    np.save(f"data/test_mode_bispectra_covarinace_p_{p_max}_Nk_{Nk}.npy", basis.mode_bispectra_covariance)
 
-    '''
     shapes = ["local", "equilateral", "orthogonal"]
     n_s, A_s = basis.parameter_n_scalar, basis.parameter_A_scalar
     print(n_s, A_s)
@@ -29,5 +32,4 @@ for Nk in Nks:
 
     df = basis.constrain_models(models)
 
-    df.to_csv(f"data/trio_constraints_p_{p_max}_Nk_{Nk}.csv", float_format="%.18e")
-    '''
+#    df.to_csv(f"data/test_trio_constraints_p_{p_max}_Nk_{Nk}.csv", float_format="%.18e")